a80e338f92d6a2e943e959c449debab2f237c85f
jaguar-ks <fahdamineksouss@gmail.com>
2026-04-14 22:56:30 +0100

    test(cucumber): add JUnit runner and align test dependencies

--- src/test/java/oncf/runners/TestRunner.java
+++ src/test/java/oncf/runners/TestRunner.java
@@ -1,5 +1,23 @@
 package oncf.runners;
 
-public class TestRunner {
+import org.junit.runner.RunWith;
 
-}
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/java/resources/features",
+    glue = {
+        "oncf.steps",
+        "oncf.base"
+    },
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/report.html",
+        "json:target/cucumber-reports/report.json"
+    },
+    monochrome = true,
+    dryRun = false
+)
+public class TestRunner {}
